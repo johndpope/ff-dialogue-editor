22b96f4e4d67a0d3247323ad179dcd016a667bd9
N. van Oosten <nvanoosten.eastcreations@gmail.com>
2016-03-03 14:45:29 +0100

    Renamed folders
    Styles and scripts folders are renamed to css and js respectively.

--- js/base.js
+++ js/base.js
@@ -7,8 +7,8 @@ var graph = new joint.dia.Graph;
 
 var paper = new joint.dia.Paper({
     el: $('#main-graph'),
-    width: 600,
-    height: 200,
+    width: "100%",
+    height: "100%",
     model: graph,
     gridSize: 1
 });
@@ -29,6 +29,8 @@ var link = new joint.dia.Link({
 
 graph.addCells([rect, rect2, link]);
 
-graph.on('all', function (eventName, cell) {
-    console.log(arguments);
-});
+$(paper.svg).attr('viewBox', '0 0 500 500');
+$(paper.svg).attr('preserveAspectRatio', 'xMinYMin meet');
+$(paper.svg).addClass('svg-content');
+$(paper.svg).removeAttr('width');
+$(paper.svg).removeAttr('height');
